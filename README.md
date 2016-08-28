36c70cc82de0d8130f1175b67eef11a3b0414f57
Mosè Giordano <mose@gnu.org>
2016-08-28 20:17:40 +0200

    Implement the algorithm

--- README.md
+++ README.md
@@ -1,3 +1,43 @@
 # Wiener
 
 [![Build Status](https://travis-ci.org/giordano/Wiener.jl.svg?branch=master)](https://travis-ci.org/giordano/Wiener.jl)
+
+[Wiener deconvolution](https://en.wikipedia.org/wiki/Wiener_deconvolution) in
+[Julia language](http://julialang.org/).
+
+``` julia
+using Images, TestImages, Wiener, ImageView
+
+# Open the image
+img = float(data(testimage("cameraman")))'
+# Create the blurring kernel in frequency domain
+x = hcat(ntuple(x -> collect((1:512) - 257), 512)...)
+k = 0.001
+blurring_ft = exp(-k*(x .^ 2 + x' .^ 2).^(5//6))
+# Create additive noise
+noise = rand(size(img))
+# Fourier transform of the blurred image, with additive noise
+blurred_img_ft = fftshift(blurring_ft) .* fft(img) + fft(noise)
+# Get the blurred image from its Fourier transform
+blurred_img = real(ifft(blurred_img_ft))
+# Get the blurring kernel in the space domain
+blurring = ifft(fftshift(blurring_ft))
+# Polish the image with Wiener deconvolution
+polished = wiener(blurred_img, img, noise, blurring)
+
+# Wiener deconvolution works also when you don't have the real image and noise,
+# that is the most common and useful case.  This happens because the Wiener
+# filter only cares about the power spectrum of the signal and the noise, so you
+# don't need to have the exact signal and noise but something with a similar
+# power spectrum.
+img2 = float(data(testimage("livingroom"))) # Load another image
+noise2 = rand(size(img)) # Create another additive noise
+# Polish the image with Wiener deconvolution
+polished2 = wiener(blurred_img, img2, noise2, blurring)
+
+# Compare...
+view(img) # ...the original image
+view(blurred_img) # ...the blurred image
+view(polished) # ...the polished image
+view(polished2) # ...the second polished image
+```
--- src/Wiener.jl
+++ src/Wiener.jl
@@ -1,5 +1,80 @@
+### Wiener.jl ---  Wiener deconvolution
+#
+# Copyright (C) 2016 Mosè Giordano.
+#
+# Maintainer: Mosè Giordano <mose AT gnu DOT org>
+# Keywords: wiener, deconvolution, digital signals
+#
+# This file is a part of Wiener.jl.
+#
+# License is MIT "Expat".
+#
+### Code:
+
+__precompile__()
+
 module Wiener
 
-# package code goes here
+export wiener
+
+### The implementation of the algorithm
+
+function _no_blur(Y::AbstractArray, # Fourier transform of the input
+                  S::AbstractArray, # Power spectrum of the signal
+                  N::AbstractArray) # Power spectrum of the noise
+    # Without blurring, the filter is:
+    #   |S|² / (|S|² + |N|²)
+    return real(ifft(Y .* S ./ (S .+ N)))
+end
+
+function _with_blur(Y::AbstractArray, # Fourier transform of the input
+                    S::AbstractArray, # Power spectrum of the signal
+                    N::AbstractArray, # Power spectrum of the noise
+                    H::AbstractArray) # Fourier transform of the blurring function
+    # With blurring, the filter is:
+    #   H* / (|H|² + |N/S|²)
+    return real(ifft(Y .* conj(H) ./ (abs2(H) .+ N ./ S)))
+end
+
+### User interface
+
+function wiener(input::AbstractArray, signal::AbstractArray,
+                noise::AbstractArray)
+    @assert size(input) == size(signal) == size(noise)
+    input_ft = fft(input)
+    signal_power_spectrum = abs2(fft(signal))
+    noise_power_spectrum = abs2(fft(noise))
+    return _no_blur(input_ft, signal_power_spectrum, noise_power_spectrum)
+end
+
+wiener(input::AbstractArray, signal::AbstractArray, noise::Real) =
+    wiener(input, signal, ones(input)*noise)
+
+function wiener(input::AbstractArray, signal::AbstractArray,
+                noise::AbstractArray, blurring::AbstractArray)
+    @assert size(input) == size(signal) == size(noise) == size(blurring)
+    input_ft = fft(input)
+    signal_power_spectrum = abs2(fft(signal))
+    noise_power_spectrum = abs2(fft(noise))
+    blurring_ft = fft(blurring)
+    return _with_blur(input_ft, signal_power_spectrum,
+                      noise_power_spectrum, blurring_ft)
+end
+
+wiener(input::AbstractArray, signal::AbstractArray,
+       noise::Real, blurring::AbstractArray) =
+           wiener(input, signal, ones(input)*noise, blurring)
+
+"""
+    wiener(input, signal, noise[, blurring])
+
+Return the Wiener deconvolution of `input`, using the power spectrum of `signal`
+and `noise`.  If the `input` was blurred with a known blurring function, pass it
+as fourth argument, `blurring`.
+
+All arguments must be in the time/space domain, they will be converted into the
+frequency domain internally using the `fft` function.
+"""
+wiener
 
 end # module
