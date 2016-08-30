69bbd4e4f55e1db2a7bf3433e922dbed658e6dbc
Mosè Giordano <mose@gnu.org>
2016-08-30 13:25:34 +0200

    Create Deconvolution.jl file and adapt wiener.jl

---
+++ src/Deconvolution.jl
@@ -0,0 +1,20 @@
+### Deconvolution.jl ---  Deconvolution in Julia language
+#
+# Copyright (C) 2016 Mosè Giordano.
+#
+# Maintainer: Mosè Giordano <mose AT gnu DOT org>
+# Keywords: deconvolution, signal processing
+#
+# This file is a part of Deconvolution.jl.
+#
+# License is MIT "Expat".
+#
+### Code:
+
+__precompile__()
+
+module Deconvolution
+
+include("wiener.jl")
+
+end # module
--- src/wiener.jl
+++ src/wiener.jl
@@ -3,34 +3,38 @@
 # Copyright (C) 2016 Mosè Giordano.
 #
 # Maintainer: Mosè Giordano <mose AT gnu DOT org>
-# Keywords: wiener, deconvolution, digital signals
+# Keywords: wiener, deconvolution, signal processing
 #
-# This file is a part of Wiener.jl.
+# This file is a part of Deconvolution.jl.
 #
 # License is MIT "Expat".
 #
+### Commentary:
+#
+# This file provides the `wiener' function to perform Wiener deconvolution.  It
+# has two modes: one for the case of presence of a known blurring, the other one
+# without blurring.
+#
 ### Code:
-
-__precompile__()
-
-module Wiener
 
 export wiener
 
-### The implementation of the algorithm
+### The implementation of the deconvolution algorithm
 
-function _no_blur(Y::AbstractArray, # Fourier transform of the input
-                  S::AbstractArray, # Power spectrum of the signal
-                  N::AbstractArray) # Power spectrum of the noise
+function _wiener_no_blur(Y::AbstractArray, # Fourier transform of the input
+                         S::AbstractArray, # Power spectrum of the signal
+                         N::AbstractArray) # Power spectrum of the noise
+    @assert size(Y) == size(S) == size(N)
     # Without blurring, the filter is:
     #   |S|² / (|S|² + |N|²)
     return real(ifft(Y .* S ./ (S .+ N)))
 end
 
-function _with_blur(Y::AbstractArray, # Fourier transform of the input
-                    S::AbstractArray, # Power spectrum of the signal
-                    N::AbstractArray, # Power spectrum of the noise
-                    H::AbstractArray) # Fourier transform of the blurring function
+function _wiener_with_blur(Y::AbstractArray, # Fourier transform of the input
+                           S::AbstractArray, # Power spectrum of the signal
+                           N::AbstractArray, # Power spectrum of the noise
+                           H::AbstractArray) # Fourier transform of the blurring
+    @assert size(Y) == size(S) == size(N) == size(H)
     # With blurring, the filter is:
     #   H* / (|H|² + |N/S|²)
     return real(ifft(Y .* conj(H) ./ (abs2(H) .+ N ./ S)))
@@ -40,11 +44,11 @@ end
 
 function wiener(input::AbstractArray, signal::AbstractArray,
                 noise::AbstractArray)
-    @assert size(input) == size(signal) == size(noise)
     input_ft = fft(input)
     signal_power_spectrum = abs2(fft(signal))
     noise_power_spectrum = abs2(fft(noise))
-    return _no_blur(input_ft, signal_power_spectrum, noise_power_spectrum)
+    return _wiener_no_blur(input_ft, signal_power_spectrum,
+                           noise_power_spectrum)
 end
 
 wiener(input::AbstractArray, signal::AbstractArray, noise::Real) =
@@ -52,13 +56,12 @@ wiener(input::AbstractArray, signal::AbstractArray, noise::Real) =
 
 function wiener(input::AbstractArray, signal::AbstractArray,
                 noise::AbstractArray, blurring::AbstractArray)
-    @assert size(input) == size(signal) == size(noise) == size(blurring)
     input_ft = fft(input)
     signal_power_spectrum = abs2(fft(signal))
     noise_power_spectrum = abs2(fft(noise))
     blurring_ft = fft(blurring)
-    return _with_blur(input_ft, signal_power_spectrum,
-                      noise_power_spectrum, blurring_ft)
+    return _wiener_with_blur(input_ft, signal_power_spectrum,
+                             noise_power_spectrum, blurring_ft)
 end
 
 wiener(input::AbstractArray, signal::AbstractArray,
@@ -76,5 +79,3 @@ All arguments must be in the time/space domain, they will be converted into the
 frequency domain internally using the `fft` function.
 """
 wiener
-
-end # module
