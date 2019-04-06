23bd2a2b2d21c605d533d1f951e2f2e894f2ccda
Mosè Giordano <m.giordano@ucl.ac.uk>
2019-04-06 13:26:50 +0100

    Updates examples and require Julia 1.0
    Fix #4

--- doc/wiener-cameraman.jl
+++ doc/wiener-cameraman.jl
@@ -15,20 +15,20 @@
 #
 ### Code:
 
-using Images, TestImages, Deconvolution, ImageView
+using Images, TestImages, Deconvolution, Random, FFTW
 
 Random.seed!(42)
 
 # Open the test image
-img = float(data(testimage("cameraman")))'
+img = channelview(testimage("cameraman"))
 # Create the blurring kernel in frequency domain
-x = hcat(ntuple(x -> collect((1:512) - 257), 512)...)
+x = hcat(ntuple(x -> collect((1:512) .- 257), 512)...)
 k = 0.001
-blurring_ft = exp(-k*(x .^ 2 + x' .^ 2).^(5//6))
+blurring_ft = @. exp(-k*(x ^ 2 + x ^ 2)^(5//6))
 # Create additive noise
-noise = rand(size(img))
+noise = rand(Float64, size(img))
 # Fourier transform of the blurred image, with additive noise
-blurred_img_ft = fftshift(blurring_ft) .* fft(img) + fft(noise)
+blurred_img_ft = fftshift(blurring_ft) .* fft(img) .+ fft(noise)
 # Get the blurred image from its Fourier transform
 blurred_img = real(ifft(blurred_img_ft))
 # Get the blurring kernel in the space domain
@@ -41,12 +41,12 @@ polished = wiener(blurred_img, img, noise, blurring)
 # filter only cares about the power spectrum of the signal and the noise, so you
 # don't need to have the exact signal and noise but something with a similar
 # power spectrum.
-img2 = float(data(testimage("livingroom"))) # Load another image
-noise2 = rand(size(img)) # Create another additive noise
+img2 = channelview(testimage("livingroom")) # Load another image
+noise2 = rand(Float64, size(img)) # Create another additive noise
 # Polish the image with Deconvolution deconvolution
 polished2 = wiener(blurred_img, img2, noise2, blurring)
 
 out = ((vcat(hcat(img, ones(512, 20), blurred_img),
              ones(20, 1024+20),
              hcat(polished, ones(512, 20), polished2))))
-save("wiener-cameraman.jpg", map(Images.Clamp01NaN(out), out))
+save("wiener-cameraman.jpg", Images.clamp01nan.(out))
