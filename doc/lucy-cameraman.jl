fe60f378774760960cc32553dd3459d12019eef1
Jakub Wronowski <jakubwro@gmail.com>
2019-12-01 19:14:16 +0100

    More spectacular lucy example

--- doc/lucy-cameraman.jl
+++ doc/lucy-cameraman.jl
@@ -1,54 +1,24 @@
-### lucy-cameraman.jl
-#
-# Copyright (C) 2019 Jakub Wronowski.
-#
-# Maintainer: Mosè Giordano <mose AT gnu DOT org>
-# Keywords: deconvolution, signal processing
-#
-# This file is a part of Deconvolution.jl.
-#
-# License is MIT "Expat".
-#
-### Commentary:
-#
-# This file produces an example image used in Deconvolution.jl documentation.
-#
-### Code:
-
-using Images, TestImages, Deconvolution, Random, FFTW
-
-Random.seed!(42)
+using Images, TestImages
+using Deconvolution
+using FFTW
+using ZernikePolynomials #https://github.com/rdoelman/ZernikePolynomials.jl
 
 img = channelview(testimage("cameraman"))
 
-(N, M) = size(img)
-@assert N == M
-@assert ispow2(N) # ensure size is power of 2
+blurring = evaluateZernike(LinRange(-16,16,512), [12, 4, 0], [1.0, -1.0, 2.0], index=:OSA)
+blurring = blurring ./ maximum(blurring)
+blurring = fftshift(blurring)
+blurring = blurring ./ sum(blurring)
 
-x = reshape(repeat(-N÷2:N÷2-1, M), (N,M))
-k = 0.001
+blurred_img = fft(img) .* fft(blurring) |> ifft |> real
 
-blurring_ft = @. exp(-k*(x ^ 2 + x ^ 2)^(5//6))
-blurred_img_ft = fftshift(blurring_ft) .* fft(img)
-blurred_img = ifft(blurred_img_ft) |> real
-blurring = ifft(fftshift(blurring_ft)) |> real
+@time result_200_iterations = lucy(blurred_img, blurring, iterations=200)
+@time result_2000_iterations = lucy(blurred_img, blurring, iterations=2000)
 
-exact_psf_result = lucy(blurred_img, blurring, iterations=100)
-
-k=0.0007 # try a bit different blur
-noise = rand(size(x)...)./20 # and add noise
-blurred_img_ft = fftshift(blurring_ft) .* fft(img) .+ fft(noise)
-blurred_img = ifft(blurred_img_ft) |> real
-blurring_ft = @. exp(-k*(x ^ 2 + x ^ 2)^(5//6))
-blurring = ifft(fftshift(blurring_ft)) |> real
-
-different_psf_result = lucy(blurred_img, blurring, iterations=100)
+(N, M) = size(blurred_img)
 
 out = ((vcat(hcat(img, ones(M, 20), blurred_img),
              ones(20, 2M+20),
-             hcat(exact_psf_result, ones(M, 20), different_psf_result))))
+             hcat(result_200_iterations, ones(M, 20), result_2000_iterations))))
 
 save("lucy-cameraman.jpg", Images.clamp01nan.(out))
-
-
-
